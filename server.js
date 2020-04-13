33a9b23c9500831c813a711b897ab31cdb794dbd
jasonwongys <jasonwongys@hotmail.com>
2020-04-14 01:21:55 +0800

    deploy

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
     })
 );
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "CLIENT", "build")))
 
 
 //DB config
